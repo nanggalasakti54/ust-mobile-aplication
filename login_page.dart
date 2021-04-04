c8422b587c4c9e632e6c07699a2d6e1369c58ccf
nanggalasakti54 <42232103+nanggalasakti54@users.noreply.github.com>
2021-04-04 21:59:21 +0800

    update text field

--- login_page.dart
+++ login_page.dart
@@ -39,7 +39,7 @@ class LoginPage extends StatelessWidget {
   Widget _iconLogin() {
     return Image.asset(
       "assets/image/icon_login.png";
-      widht:150.0,
+      width:150.0,
       height:150.0
     );
   }
@@ -48,8 +48,80 @@ class LoginPage extends StatelessWidget {
     return Column (
       children: <Widget> [
         Padding (
-          padding: EdgeInsets.only(top:16.0)
-        )
+          padding: EdgeInsets.only(top:16.0),
+        ),
+        Text (
+          "Login into app",
+          style: TextStyle (
+            color: Colors.white,
+            fontSize:16.0,
+          ),
+        ),
+        Padding (
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        Text (
+          "ini adalah home page",
+          style: TextStyle (
+            fontSize: 12.0,
+            color: Colors.white,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  Widget _textField() {
+    return Column (
+      children: <Widget> [
+        Padding (
+          padding: EdgeInsets.only(top: 12.0),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            border: underlineInputBorder(),
+            enableBorder: underlineInputBorder(
+              borderSide: BorderSide(
+                color: ColorPalette.underlineTextField,
+                width: 1.5,
+              ),
+            ),
+            focusedBorder: underlineInputBorder(
+              borderSide: BorderSide (
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            hintText: "Username",
+            hintStyle: TextStyle (color: ColorPalette.hintColor),
+          ),
+          style: TextStyle(color: Colors.white),
+          autofocus: false,
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 12,0),
+        ),
+        TextFormField (
+          decoration: const InputDecoration(
+            border: underlineInputBorder(),
+            enableBorder: borderSide(
+              color: ColorPalette.underlineTextField,
+              width: 1.5,
+            ),
+          ),
+          focusedBorder: underlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.white,
+              width: 3.0,
+            ),
+          ),
+          hintText: "Password",
+          hintStyle: TextStyle (color: ColorPalette.hintColor),
+        ),
+        style: TextStyle(color: Colors.white),
+        obscureText: true,
+        autofocus: false,
       ]
     )
   }
